f70a574886bee6cc755c58a071d0a103c4bbf83a
Prasanga Dhakal <prasanga.dhakal@gmail.com>
2020-08-30 07:54:29 -0230

    fadein animation on pageload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*
 	Read Only by HTML5 UP
 	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Free for perso nal and commercial use under the CCA3.0 license (html5up.net/license)
 */
 
 (function($) {
